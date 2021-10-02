89121c5db15df0c18c49c6617d1e4b83190b9613
Pascal Clarkson <info@artocraft.nl>
2021-10-02 00:37:45 +0000

    moved penalty box to board

--- game.py
+++ game.py
@@ -6,29 +6,37 @@ class Player:
     def __init__(self, name: str):
         self.name = name
         self.purse = 0
-        self.in_penalty_box = False
 
 
 class Board:
     def __init__(self):
         self.pawn_location = dict()
+        self.pawn_penalty_box = dict()
 
-    def place_pawn(self, pawn: int, place: int = 0):
+    def place_pawn(self, pawn: int, place: int = 0) -> None:
         self.pawn_location[pawn] = place
 
-    def move_pawn(self, pawn: int, steps: int):
+    def move_pawn(self, pawn: int, steps: int) -> int:
         self.pawn_location[pawn] = (self.pawn_location[pawn] + steps) % 12
         return self.pawn_location[pawn]
 
-    def get_pawns_category(self, pawn: int):
+    def get_pawns_category(self, pawn: int) -> str:
         return self.get_category(self.pawn_location[pawn])
 
-    def get_category(self, place: int):
+    def get_category(self, place: int) -> str:
         if place == 3 or place == 7 or place == 11: return "Rock"
         if place == 0 or place == 4 or place == 8: return "Pop"
         if place == 1 or place == 5 or place == 9: return "Science"
         if place == 2 or place == 6 or place == 10: return "Sports"
 
+    def put_pawn_in_penalty_box(self, pawn: int) -> None:
+        self.pawn_penalty_box[pawn] = True
+
+    def remove_pawn_from_penalty_box(self, pawn: int) -> None:
+        self.pawn_penalty_box[pawn] = False
+
+    def is_pawn_in_penalty_box(self, pawn: int) -> bool:
+        return self.pawn_penalty_box[pawn]
 
 class QuestionsDecks:
     def __init__(self):
@@ -37,11 +45,11 @@ class QuestionsDecks:
         self.sports_questions = deque()
         self.rock_questions = deque()
 
-        for i in range(50):
-            self.pop_questions.append(self.create_question("Pop", i))
-            self.science_questions.append(self.create_question("Science", i))
-            self.sports_questions.append(self.create_question("Sports", i))
-            self.rock_questions.append(self.create_question("Rock", i))
+        for counter in range(50):
+            self.pop_questions.append(self.create_question("Pop", counter))
+            self.science_questions.append(self.create_question("Science", counter))
+            self.sports_questions.append(self.create_question("Sports", counter))
+            self.rock_questions.append(self.create_question("Rock", counter))
 
     def create_question(self, category: str, index: int) -> str:
         return f"{category} Question {index}"
@@ -70,6 +78,7 @@ class Game:
 
     def add_player(self, player_name: str) -> bool:
         self.board.place_pawn(self.player_count())
+        self.board.remove_pawn_from_penalty_box(self.player_count())
         self.players.append(Player(player_name))
         print(f"{player_name} was added.\nThey are player number {self.player_count()}.")
         return True
@@ -79,15 +88,15 @@ class Game:
 
     def roll(self, roll: int) -> None:
         print(f"{self.get_current_player().name} is the current player.\nThey have rolled a {roll}.")
+        self.is_getting_out_of_penalty_box = False
 
-        if self.get_current_player().in_penalty_box:
+        if self.board.is_pawn_in_penalty_box(self.current_player_index):
             if roll % 2 != 0:
                 print(f"{self.get_current_player().name} is getting out of the penalty box.")
                 self.is_getting_out_of_penalty_box = True
                 self.move_player_and_ask_question(roll)
             else:
                 print(f"{self.get_current_player().name} is not getting out of the penalty box.")
-                self.is_getting_out_of_penalty_box = False
         else:
             self.move_player_and_ask_question(roll)
 
@@ -106,21 +115,20 @@ class Game:
     def check_if_current_player_has_won(self):
         return self.get_current_player().purse == self.amount_of_coins_to_win
 
+    def check_for_win_or_make_next_player_current(self):
+        current_player_won = self.check_if_current_player_has_won()
+        if current_player_won:
+            print (f"{self.get_current_player().name} has won!!")
+        self.next_players_turn()
+        return current_player_won
+
     def correctly_answered(self) -> bool:
-        if self.get_current_player().in_penalty_box and not self.is_getting_out_of_penalty_box:
-            self.next_players_turn()
-            return False
-        else:
+        if not self.board.is_pawn_in_penalty_box(self.current_player_index) or self.is_getting_out_of_penalty_box:
             print("Answer was correct!!!!")
             self.current_player_gains_coin()
-            current_player_won = self.check_if_current_player_has_won()
-            if current_player_won:
-                print (f"{self.get_current_player().name} has won!!")
-            self.next_players_turn()
-            return current_player_won
+        return self.check_for_win_or_make_next_player_current()
 
     def incorrectly_answered(self) -> bool:
         print(f"Question was incorrectly answered.\n{self.get_current_player().name} was sent to the penalty box.")
-        self.get_current_player().in_penalty_box = True
-        self.next_players_turn()
-        return False
+        self.board.put_pawn_in_penalty_box(self.current_player_index)
+        return self.check_for_win_or_make_next_player_current()
