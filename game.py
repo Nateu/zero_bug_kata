dd40821abed0d0afb5bc7f96b79511f8a9f4b9c6
Pascal Clarkson <info@artocraft.nl>
2021-10-03 10:25:15 +0000

    Some readability edits

--- game.py
+++ game.py
@@ -19,7 +19,7 @@ class Player:
 
 
 class Board:
-    def __init__(self, size: int = 12, categories: [str] = ["Pop", "Science", "Sports", "Rock"]) -> None:
+    def __init__(self, size: int = 12, categories: List[str] = ["Pop", "Science", "Sports", "Rock"]) -> None:
         self.pawn_location = dict()
         self.pawn_penalty_box = dict()
         self.categories = categories
@@ -48,12 +48,12 @@ class Board:
     def is_pawn_in_penalty_box(self, pawn: int) -> bool:
         return self.pawn_penalty_box[pawn]
 
-    def list_all_categories(self) -> [str]:
+    def list_all_categories(self) -> List[str]:
         return self.categories
 
 
 class QuestionsDecks:
-    def __init__(self, categories: [str]) -> None:
+    def __init__(self, categories: List[str]) -> None:
         self.decks = dict()
         for category in categories:
             self.decks[category] = deque()
@@ -74,21 +74,26 @@ class QuestionsDecks:
 class Game:
     def __init__(self, coins_to_win: int = 6, max_players: int = 6) -> None:
         self.amount_of_coins_to_win = coins_to_win
+        self.max_players = max_players
+        self.current_player_index = -1
+        self.is_getting_out_of_penalty_box: bool = False
         self.players: List[Player] = []
         self.board = Board()
         self.questions_decks = QuestionsDecks(self.board.list_all_categories())
-        self.current_player_index = -1
-        self.is_getting_out_of_penalty_box: bool = False
-        self.max_players = max_players
 
     def add_player(self, player_name: str) -> bool:
         if self.player_count() >= self.max_players:
-            print(f"Max number of players is {self.max_players}.")
+            print(
+                f"Max number of players is {self.max_players}."
+            )
             return False
         self.board.place_pawn(self.player_count())
         self.board.remove_pawn_from_penalty_box(self.player_count())
         self.players.append(Player(player_name))
-        print(f"{player_name} was added.\nThey are player number {self.player_count()}.")
+        print(
+            f"{player_name} was added.\n" +
+            f"They are player number {self.player_count()}."
+        )
         return True
 
     def player_count(self) -> int:
@@ -96,18 +101,27 @@ class Game:
 
     def roll(self, roll: int) -> None:
         if self.player_count() < 2:
-            print(f"Min number of players is 2.")
+            print(
+                f"Min number of players is 2."
+            )
             return False
         self.set_new_current_player()
-        print(f"{self.get_current_player()} is the current player.\nThey have rolled a {roll}.")
+        print(
+            f"{self.get_current_player()} is the current player.\n" +
+            f"They have rolled a {roll}."
+        )
 
         if self.board.is_pawn_in_penalty_box(self.current_player_index):
-            if roll % 2 != 0:
-                print(f"{self.get_current_player()} is getting out of the penalty box.")
+            if roll % 2 == 0:
+                print(
+                    f"{self.get_current_player()} is not getting out of the penalty box."
+                )
+            else:
+                print(
+                    f"{self.get_current_player()} is getting out of the penalty box."
+                )
                 self.is_getting_out_of_penalty_box = True
                 self.move_player_and_ask_question(roll)
-            else:
-                print(f"{self.get_current_player()} is not getting out of the penalty box.")
         else:
             self.move_player_and_ask_question(roll)
 
@@ -124,27 +138,40 @@ class Game:
     def move_player_and_ask_question(self, roll: int) -> None:
         current_place = self.board.move_pawn(self.current_player_index, roll)
         current_category = self.board.get_pawns_category(self.current_player_index)
-        print(f"{self.get_current_player()}'s new location is {current_place}.\nThe category is {current_category}.\n{self.questions_decks.get_question(current_category)}.")
+        print(
+            f"{self.get_current_player()}'s new location is {current_place}.\n" +
+            f"The category is {current_category}.\n" +
+            f"{self.questions_decks.get_question(current_category)}."
+        )
 
     def correctly_answered(self) -> bool:
         if not self.board.is_pawn_in_penalty_box(self.current_player_index) or self.is_getting_out_of_penalty_box:
-            print("Answer was correct!!!!")
+            print(
+                "Answer was correct!!!!"
+            )
             self.current_player_gains_coin()
             self.board.remove_pawn_from_penalty_box(self.current_player_index)
         return self.finish_turn()
 
     def current_player_gains_coin(self) -> None:
-        print(f"{self.get_current_player()} now has {self.get_current_player().add_coins()} Gold Coins.")
+        print(
+            f"{self.get_current_player()} now has {self.get_current_player().add_coins()} Gold Coins."
+        )
 
     def finish_turn(self) -> bool:
         if self.check_if_current_player_has_won():
-            print (f"{self.get_current_player()} has won!!")
+            print (
+                f"{self.get_current_player()} has won!!"
+            )
         return self.check_if_current_player_has_won()
 
     def check_if_current_player_has_won(self) -> bool:
         return self.get_current_player().get_wealth() == self.amount_of_coins_to_win
 
     def incorrectly_answered(self) -> bool:
-        print(f"Question was incorrectly answered.\n{self.get_current_player()} was sent to the penalty box.")
+        print(
+            f"Question was incorrectly answered.\n" +
+            f"{self.get_current_player()} was sent to the penalty box."
+        )
         self.board.put_pawn_in_penalty_box(self.current_player_index)
         return self.finish_turn()
