a8b56925f8cd1d38e776d2bcf73268e4c24c13cc
Pascal Clarkson <info@artocraft.nl>
2021-10-06 15:29:00 +0200

    Removed unneeded code due to approval test with seed

--- game_runner.py
+++ game_runner.py
@@ -2,12 +2,6 @@ from random import randrange, seed
 from game import Game
 
 
-def write_log(s: str):
-    with open("command.log", "a") as log_file:
-        log_file.write(s)
-        log_file.write("\r")
-
-
 if __name__ == '__main__':
 
     not_a_winner = False
@@ -15,24 +9,18 @@ if __name__ == '__main__':
     game = Game()
 
     game.add('Chet')
-    write_log("game.add('Chet')")
     game.add('Pat')
-    write_log("game.add('Pat')")
     game.add('Sue')
-    write_log("game.add('Sue')")
 
     seed(11)
 
     while True:
         roll = randrange(5) + 1
         game.roll(roll)
-        write_log(f"game.roll({roll})")
 
         if randrange(9) == 7:
             not_a_winner = game.wrong_answer()
-            write_log("game.wrong_answer()")
         else:
             not_a_winner = game.was_correctly_answered()
-            write_log("game.was_correctly_answered()")
-        
+
         if not not_a_winner: break
